6271a2cf3d30d253e988989edc078063e05f2125
Yair <yairpro@gmail.com>
2026-06-15 15:26:32 +0200

    Enhance keyword functionality

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,13 +1,29 @@
 'use strict'
 
-function renderGallery(keyword = '') {
-    const images = getImages(keyword)
+let gKeyword = ''
+
+function renderGallery(gKeyword) {
+    const images = getImages(gKeyword)
     var imagesStr = ``
 
     const imgStrs = images.map(({id, url}) => {
-        return `<img src="${url}" alt="img-${id}" onclick="onImgSelect(${id})">`
+        return `<div class="img-container"><img src="${url}" alt="img-${id}" onclick="onImgSelect(${id})"></div>`
     })
 
     const elGalleryCtnr = document.querySelector('.gallery-container')
     elGalleryCtnr.innerHTML = imgStrs.join('')
 }
+
+function renderKeywords(numKeywords) {
+    const sizes = getKeyword2Size().slice(0, numKeywords)
+    const sizesStrs = sizes.map(sizeMap => {
+        const keyword = sizeMap[0]
+        const emSize = sizeMap[1]
+        return `<span class="keyword" style="font-size: ${emSize}em">${keyword}</span>`
+    })
+    
+    const elKeywords = document.querySelector('.gallery-keywords')
+    elKeywords.innerHTML = sizesStrs.join('')
+    
+}
+
